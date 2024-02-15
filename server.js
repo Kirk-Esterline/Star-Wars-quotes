18f729997c9b21dfbf0911fdc3fd9ab9d4c74df4
Capn-Cricket <111900268+Capn-Cricket@users.noreply.github.com>
2024-02-15 18:05:13 -0500

    Further developed the server side capability of the program to store data in a database

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
 
 app.listen(3000)
+
+MongoClient.connect('mongodb+srv://kirk:X6ua8gVN4ebQsLzt@cluster0.2c4ae6s.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true })
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('cluster0')
+        const quotesCollection = db.collection('quotes')
+        // app.use(/*--*/)
+        // app.get(/*--*/)
+        app.post('/quotes', (req, res) => {
+            quotesCollection
+                .insertOne(req.body)
+                .then(result => {
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
+        // app.listen(/*--*/)
+    })
+    .catch(error => console.error(error))
 
 //Make sure to place body-parser berfor the CRUD handlers!
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -12,6 +32,5 @@ app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-})
+
+
